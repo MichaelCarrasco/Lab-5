a9b0f943f2be3215c2716f1858b43821cdd131e9
MichaelCarrasco <67906994+MichaelCarrasco@users.noreply.github.com>
2020-08-06 21:09:28 -0400

    Update StringReversal.java

--- Lab 5/src/StringReversal.java
+++ Lab 5/src/StringReversal.java
@@ -4,13 +4,13 @@ public class StringReversal {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String s=input.nextLine();
-		System.out.println(reverse(s));
+		String s=input.nextLine(); //string input
+		System.out.println(reverse(s)); //prints out our reversed string
 		input.close();
 		
 
 	}
-	public static String reverse(String str) {
+	public static String reverse(String str) { //takes the first character of the string and outputs it until no characters are left.
 	    if ((null == str) || (str.length() <= 1)) {
 	        return str;
 	    }
